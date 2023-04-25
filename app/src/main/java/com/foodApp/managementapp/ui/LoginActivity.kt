612182c55b24955f982250f73ce1d628265b708f
koustavroy <koustav.roy@myelinfoundry.com>
2023-04-25 23:50:15 +0530

    Login page added

---
+++ app/src/main/java/com/foodApp/managementapp/ui/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.foodApp.managementapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodApp.managementapp.R
+import com.foodApp.managementapp.base.BaseActivity
+import com.foodApp.managementapp.databinding.ActivityLoginBinding
+import com.foodApp.managementapp.databinding.ActivityMainBinding
+import com.foodApp.managementapp.viewmodels.MainViewModel
+
+class LoginActivity :  BaseActivity<ActivityLoginBinding, MainViewModel>(
+    MainViewModel::class.java,
+    { inflater -> ActivityLoginBinding.inflate(inflater) }
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setupViews() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/foodApp/managementapp/viewmodels/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.foodApp.managementapp.viewmodels
+
+import com.foodApp.managementapp.base.BaseRepository
+import com.foodApp.managementapp.base.BaseViewModel
+
+class LoginViewModel(private val repository: BaseRepository): BaseViewModel(){
+}
